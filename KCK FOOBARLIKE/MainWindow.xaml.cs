8a6985609ea0d267be33c52685f9c84eed4751c2
miikhaiil <73438524+miikhaiil@users.noreply.github.com>
2021-06-07 15:33:13 +0200

    ADD

---
+++ KCK FOOBARLIKE/MainWindow.xaml.cs
@@ -0,0 +1,445 @@
+﻿using Microsoft.Win32;
+using System.Threading;
+using System.Windows;
+using System.Windows.Input;
+using Ookii.Dialogs.Wpf;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace KCK_OKIENKO
+{
+    public class Song
+    {
+        public string name { get; set; }
+        public string duration { get; set; }
+    }
+
+    public partial class MainWindow : Window
+    {
+        Player player;
+        Thread thread;
+        public void startAutoPlay()
+        {
+            thread = new Thread(() => autoPlay());
+            thread.Start();
+        }
+
+        public void autoPlay()
+        {
+            while (true)
+            {
+                if (player.flag == true)
+                {
+                    player.checkTime();
+                }
+                Thread.Sleep(100);
+            }
+        }
+
+        private List<String> GetFiles(string path, string pattern)
+        {
+            var files = new List<string>();
+            var directories = new string[] { };
+
+            try
+            {
+                files.AddRange(Directory.GetFiles(path, pattern, SearchOption.TopDirectoryOnly));
+                directories = Directory.GetDirectories(path);
+            }
+            catch (UnauthorizedAccessException) { }
+
+            foreach (var directory in directories)
+                try
+                {
+                    files.AddRange(GetFiles(directory, pattern));
+                }
+                catch (UnauthorizedAccessException) { }
+
+            return files;
+        }
+
+        public MainWindow()
+        {
+            player = Player.getInstance(this);
+            InitializeComponent();
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+            ItemsContainer.Foreground = new SolidColorBrush(Colors.Orange);
+            startAutoPlay();
+
+        }
+
+        private void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            this.DragMove();
+        }
+
+        private void CloseClick(object sender, RoutedEventArgs e)
+        {
+            if(player.isPlaying)
+            {
+                player.Stop();
+            }
+            thread.Abort();
+            Close();
+        }
+
+        private void MaximizeClick(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+            {
+                WindowState = WindowState.Maximized;
+            }
+            else
+                WindowState = WindowState.Normal;
+        }
+
+        private void MinimalizeClick(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void onAddFilesClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Mp3 files (*.mp3)|*.mp3";
+            List<string> files = new List<string>();
+            int counter=0;
+            dialog.Multiselect = true;
+            if (dialog.ShowDialog() == true)
+            {
+                foreach (string path in dialog.FileNames)
+                {
+                    player.addToPlaylist(path);
+                    files.Add(path);
+                }
+                while(ItemsContainer.Items.Count>0)
+                {
+                    ItemsContainer.Items.RemoveAt(0);
+                }
+                for (int i = 0; i < player.getPlaylistSize(); i++)
+                {
+                    ItemsContainer.Items.Add(new Song() { name = player.getName(i), duration = player.getSongTime(i).ToString().Substring(3, 5) });
+                }
+                
+            }  
+        }
+
+        private void onAddFolderButtonClick(object sender, RoutedEventArgs e)
+        {
+            List<String> paths = null;
+            VistaFolderBrowserDialog dialog = new VistaFolderBrowserDialog();
+            if(dialog.ShowDialog() == true)
+            {
+                paths = GetFiles(dialog.SelectedPath, "*.mp3");
+                if (paths.Count != 0)
+                {
+                    foreach (string path in paths)
+                    {
+                        player.addToPlaylist(path);
+                    }
+                    while (ItemsContainer.Items.Count > 0)
+                    {
+                        ItemsContainer.Items.RemoveAt(0);
+                    }
+                    for (int i = 0; i < player.getPlaylistSize(); i++)
+                    {
+                        ItemsContainer.Items.Add(new Song() { name = player.getName(i), duration = player.getSongTime(i).ToString().Substring(3, 5) });
+                    }
+                }
+                else
+                    MessageBox.Show("There is no songs to load");
+            }
+        }
+
+        private void onLoadPlaylistButtonClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Playlist file (*.plr)|*.plr";
+            dialog.Multiselect = true;
+            ItemsContainer.SelectedIndex = -1;
+            if (dialog.ShowDialog() == true)
+            {
+                while (ItemsContainer.Items.Count > 0)
+                {
+                    ItemsContainer.Items.RemoveAt(0);
+                }
+                foreach (string path in dialog.FileNames)
+                {
+                    player.loadPlaylist(path);
+                }
+                for (int i = 0; i < player.getPlaylistSize(); i++)
+                {
+                    ItemsContainer.Items.Add(new Song() { name = player.getName(i), duration = player.getSongTime(i).ToString().Substring(3, 5) });
+                }
+                    
+            }
+        }
+
+        private void onSavePlaylistButtonClick(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Playlist file (*.plr)|*.plr";
+            if(dialog.ShowDialog() == true)
+            {
+                File.WriteAllLines(dialog.FileName, player.getPlaylist());
+            }
+        }
+
+        private void onStopButtonClick(object sender, RoutedEventArgs e)
+        {
+            CurrentTime.Content = "00:00";
+            player.Stop();
+        }
+
+        private void SelectItem(object sender, SelectionChangedEventArgs e)
+        {
+            if (ItemsContainer.SelectedIndex != -1)
+            {
+                SongTime.Content = player.getSongTime(player.currentSong).ToString().Substring(3, 5);
+            }
+            else
+            {
+                SongTime.Content = "";
+                SongName.Content = "";
+                return;
+            }
+                
+            if (!player.isPaused)
+            {
+                player.currentSong = ItemsContainer.SelectedIndex-1;
+                player.playNext();
+            }
+            else
+                player.currentSong = ItemsContainer.SelectedIndex-1;
+           
+        }
+
+        private void onPlayButtonClick(object sender, RoutedEventArgs e)
+        {
+            if(player.getPlaylistSize()>0)
+            {
+                if (ItemsContainer.SelectedIndex == -1)
+                {
+                    ItemsContainer.SelectedIndex = 0;
+                    player.Play();
+                }
+
+                if (ItemsContainer.SelectedIndex >= 0)
+                {
+                    if (!player.isPlaying)
+                    {
+                        CurrentTime.Content = "00:00";
+                        player.Play();
+                    }
+                    else if (player.isPlaying && !player.isPaused)
+                    {
+                        player.choosePlay();
+
+                    }
+                    else if (player.isPlaying && player.isPaused)
+                    {
+                        player.Play();
+                    }
+                }
+            }
+        }
+
+        private void onPauseButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (player.isPlaying)
+            {
+                player.Play();
+            }
+            else
+                return;
+        }
+
+        private void onPreviousButtonClick(object sender, RoutedEventArgs e)
+        {
+            if(player.getPlaylistSize()>0)
+            {
+                if (ItemsContainer.SelectedIndex == -1)
+                {
+                    ItemsContainer.SelectedIndex = ItemsContainer.Items.Count;
+                    player.Play();
+                }
+                if (player.isPlaying)
+                {
+                    player.playPrevious();
+                    ItemsContainer.SelectedIndex = player.currentSong;
+                }
+                else
+                {
+                    player.playPrevious();
+                    ItemsContainer.SelectedIndex = player.currentSong;
+                }
+            }
+        }
+
+        private void onNextButtonClick(object sender, RoutedEventArgs e)
+        {
+            if(player.getPlaylistSize()>0)
+            {
+                if (ItemsContainer.SelectedIndex == -1)
+                {
+                    ItemsContainer.SelectedIndex = 1;
+                    player.currentSong = 0;
+                    player.Play();
+                }
+                if (player.isPlaying)
+                {
+                    player.playNext();
+                    ItemsContainer.SelectedIndex = player.currentSong;
+                }
+                else
+                {
+                    player.playNext();
+                    ItemsContainer.SelectedIndex = player.currentSong;
+                }
+            }
+        }
+
+        private void onLoopButtonClick(object sender, RoutedEventArgs e)
+        {
+            player.isLooped = !player.isLooped;
+            if(player.isLooped==true)
+            {
+                player.isShuffled = false;
+                Loop.Background = new SolidColorBrush(Colors.Gray);
+                Shuffle.Background = null;
+            }
+            else
+            {
+                Loop.Background = null;
+            }
+            
+
+        }
+
+        private void onShuffleButtonClick(object sender, RoutedEventArgs e)
+        {
+            player.isShuffled = !player.isShuffled;
+            if (player.isShuffled == true)
+            {
+                player.isLooped = false;
+                Loop.Background = null;
+                Shuffle.Background = new SolidColorBrush(Colors.Gray);
+            }
+            else
+            {
+                Shuffle.Background = null;
+            }
+        }
+
+        private void onMuteButtonClick(object sender, RoutedEventArgs e)
+        {
+            player.setVolume(0);
+            volumeSlider.Value = 0;
+        }
+
+        private void onMaxVolButtonClick(object sender, RoutedEventArgs e)
+        {
+            player.setVolume(1);
+            volumeSlider.Value = 1;
+        }
+
+        private void volumeChanged(object sender, System.Windows.Controls.Primitives.DragDeltaEventArgs e)
+        {
+            player.setVolume((float)volumeSlider.Value);
+        }
+
+        private void timeSkipDelta(object sender, System.Windows.Controls.Primitives.DragDeltaEventArgs e)
+        {
+            player.sliderTime = timeSlider.Value;
+            CurrentTime.Content = player.currentTime.ToString().Substring(3, 5);
+            player.currentTime = TimeSpan.FromSeconds(player.sliderTime);
+
+        }
+
+        private void timeSkipStarted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
+        {
+            player.isSliding = true;
+            player.isPaused = true;
+        }
+
+        private void timeSkipCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+            player.isPaused = false;
+            player.isSliding = false;
+        }
+
+        private void onResetButtonClick(object sender, RoutedEventArgs e)
+        {
+            string name = "eq";
+            for(int i = 0; i < 10; i++)
+            {
+                var slider = (Slider)this.FindName(name + i);
+                slider.Value = 20;
+                player.setGain(i, slider.Value-20);
+                var label = (Label)this.FindName(name + i + "Label");
+                label.Content = "0dB";
+            }
+        }
+
+        private void eqChange(object sender, System.Windows.Controls.Primitives.DragDeltaEventArgs e)
+        {
+            var slider = (Slider)sender;
+            switch (slider.Name)
+            {
+                case "eq0":
+                    player.setGain(0, slider.Value - 20);
+                    eq0Label.Content = slider.Value - 20 + "dB";
+                    break;
+                case "eq1":
+
+                    player.setGain(1, slider.Value - 20);
+                    eq1Label.Content = slider.Value - 20 + "dB";
+                    break;
+                case "eq2":
+
+                    player.setGain(2, slider.Value - 20);
+                    eq2Label.Content = slider.Value - 20 + "dB";
+                    break;
+                case "eq3":
+
+                    player.setGain(3, slider.Value - 20);
+                    eq3Label.Content = slider.Value - 20 + "dB";
+                    break;
+                case "eq4":
+
+                    player.setGain(4, slider.Value - 20);
+                    eq4Label.Content = slider.Value - 20 + "dB";
+                    break;
+                case "eq5":
+
+                    player.setGain(5, slider.Value - 20);
+                    eq5Label.Content = slider.Value - 20 + "dB";
+                    break;
+                case "eq6":
+
+                    player.setGain(6, slider.Value - 20);
+                    eq6Label.Content = slider.Value - 20 + "dB";
+                    break;
+                case "eq7":
+
+                    player.setGain(7, slider.Value - 20);
+                    eq7Label.Content = slider.Value - 20 + "dB";
+                    break;
+                case "eq8":
+
+                    player.setGain(8, slider.Value - 20);
+                    eq8Label.Content = slider.Value - 20 + "dB";
+                    break;
+                case "eq9":
+
+                    player.setGain(9, slider.Value - 20);
+                    eq9Label.Content = slider.Value - 20 + "dB";
+                    break;
+            }
+        }
+    }
+}
---
+++ KCK FOOBARLIKE/Player.cs
@@ -0,0 +1,354 @@
+﻿using CSCore;
+using CSCore.Codecs;
+using CSCore.Codecs.MP3;
+using CSCore.CoreAudioAPI;
+using CSCore.MediaFoundation;
+using CSCore.SoundOut;
+using CSCore.Streams.Effects;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KCK_OKIENKO
+{
+    class Player
+    {
+        private MainWindow mainWindow;
+        private static Player instance;
+        private Player(MainWindow window)
+        {
+            this.mainWindow = window;
+            playlist = new List<string>();
+            songsTime = new List<TimeSpan>();
+            for(int i = 0; i < 10; i++)
+            {
+                gains[i] = 0d;
+            }
+        }
+
+        private List<string> playlist;
+        private List<TimeSpan> songsTime;
+
+        private ISoundOut _soundOut;
+        private static IWaveSource _soundSource;
+        private float volume = 0.5f;
+        
+        public int currentSong = 0;
+        public double sliderTime;
+
+        public TimeSpan currentTime;
+
+        //FLAGI DO WPLYWANIA NA DZIAŁANIE WĄTKU
+        public Boolean isPlaying = false;
+        public Boolean isSliding = false;
+        public Boolean isPaused = false;
+        public Boolean isLooped = false;
+        public Boolean isShuffled = false;
+        public Boolean playerWorking = false;
+        public Boolean flag = false;
+
+        private Random rnd = new Random();
+
+        private Equalizer eq;
+        private double[] gains = new double[10];
+
+        public void setGain(int i, double value)
+        {
+            gains[i] = value;
+            if (eq!=null)
+            {
+                eq.SampleFilters[i].AverageGainDB = gains[i];
+            }
+           
+        }
+
+        public TimeSpan getSongTime(int i)
+        {
+            return songsTime[i];
+        }
+
+        public void resetEqualizer()
+        {
+            int i = 0;
+            foreach (EqualizerFilter f in eq.SampleFilters)
+            {
+                f.AverageGainDB = 0;
+                gains[i] = 0;
+                i++;
+            }
+        }
+
+        public double getGain(int index)
+        {
+            return eq.SampleFilters[index].AverageGainDB;
+        }
+
+        public static Player getInstance(MainWindow window)
+        {
+            if (instance == null)
+            {
+                instance = new Player(window);
+            }
+            return instance;
+        }
+
+        public bool checkTime()
+        {
+            if (_soundOut != null && _soundOut.WaveSource != null)
+            {
+                if (isSliding)
+                {
+                    _soundOut.Pause();
+                    _soundOut.WaveSource.SetPosition(currentTime);
+                    _soundOut.Resume();
+                }
+                if (_soundOut.WaveSource.GetPosition() >= _soundOut.WaveSource.GetLength())
+                {
+                    if (isLooped)
+                    {
+                        Play();
+                    }
+                    else
+                        playNext();
+                    return true;
+                }
+                mainWindow.Dispatcher.Invoke(() => {
+                    currentTime = _soundOut.WaveSource.GetPosition();
+                    mainWindow.timeSlider.Value = _soundOut.WaveSource.GetPosition().TotalSeconds;
+                    mainWindow.CurrentTime.Content = _soundOut.WaveSource.GetPosition().ToString().Substring(3, 5);
+                });
+            }
+            return false;
+        }
+
+        public string getName(int i)
+        {
+            return Path.GetFileName(playlist[i]);
+        }
+
+        #region INICJALIZACJA
+        public void initialize(string file)
+        {
+            playerWorking = true;
+            _soundSource = CodecFactory.Instance.GetCodec(file);
+            eq = Equalizer.Create10BandEqualizer(_soundSource.ToSampleSource());
+            int i = 0;
+            foreach (EqualizerFilter f in eq.SampleFilters)
+            {
+                f.AverageGainDB = gains[i];
+                i++;
+            }
+            _soundOut = GetSoundOut();
+            _soundOut.Initialize(eq.ToWaveSource());
+            _soundOut.Volume = volume;
+        }
+
+        private static ISoundOut GetSoundOut()
+        {
+            if (WasapiOut.IsSupportedOnCurrentPlatform)
+            {
+                return new WasapiOut
+                {
+                    Device = GetDevice()
+                };
+            }
+
+            return new DirectSoundOut();
+        }
+
+        public static MMDevice GetDevice()
+        {
+            using (var mmdeviceEnumerator = new MMDeviceEnumerator())
+            {
+                using (var mmdeviceCollection = mmdeviceEnumerator.EnumAudioEndpoints(DataFlow.Render, DeviceState.Active))
+                {
+                    return mmdeviceCollection.LastOrDefault();
+                }
+            }
+        }
+        #endregion
+
+        #region ODTWARZAANIE
+        private void playPause()
+        {
+            if (_soundOut != null)
+            {
+                if (isPaused == false)
+                {
+                    _soundOut.Pause();
+                    isPaused = true;
+                }
+                else
+                {
+                    _soundOut.Resume();
+                    isPaused = false;
+                }
+            }
+        }
+
+        public void choosePlay()
+        {
+            if (currentSong >= 0 && currentSong < playlist.Count())
+            {
+                Stop();
+                initialize(playlist[currentSong]);
+                isPlaying = true;
+                mainWindow.Dispatcher.Invoke(() => {
+                    mainWindow.timeSlider.Maximum = getSongTime(currentSong).TotalSeconds;
+                    mainWindow.SongTime.Content = getSongTime(currentSong).ToString().Substring(3, 5);
+                    mainWindow.timeSlider.Value = 0;
+                    mainWindow.SongName.Content = getName(currentSong);
+                });
+                _soundOut.Play();
+                flag = true;
+            }
+        }
+        public void Play()
+        {
+            if (isPlaying == false || isLooped == true)
+            {
+                initialize(playlist[currentSong]);
+                if (_soundOut != null)
+                {
+                    mainWindow.Dispatcher.Invoke(() => {
+                        mainWindow.timeSlider.Maximum = getSongTime(currentSong).TotalSeconds;
+                        mainWindow.SongTime.Content = getSongTime(currentSong).ToString().Substring(3, 5);
+                        mainWindow.timeSlider.Value = 0;
+                        mainWindow.SongName.Content = getName(currentSong);
+                    });
+                    isPlaying = true;
+                    _soundOut.Volume = volume;
+                    _soundOut.Play();
+                    flag = true;
+                }
+            }
+            else
+            {
+                playPause();
+            }
+        }
+
+        public void Stop()
+        {
+            if (_soundOut != null)
+            {
+                flag = false;
+                _soundOut.Dispose();
+                _soundSource.Dispose();
+                isPlaying = false;
+                isPaused = false;
+            }
+        }
+        #endregion
+
+        #region SKIPOWANIE
+        public void playNext()
+        {
+            if(isShuffled)
+            {
+                currentSong = rnd.Next();
+            }
+            else
+                currentSong++;
+            currentSong = currentSong % playlist.Count;
+            mainWindow.Dispatcher.Invoke(() => {
+                mainWindow.timeSlider.Maximum = getSongTime(currentSong).TotalSeconds;
+                mainWindow.SongTime.Content = getSongTime(currentSong).ToString().Substring(3, 5);
+                mainWindow.timeSlider.Value = 0;
+                mainWindow.SongName.Content = getName(currentSong);
+            });
+            if (isPlaying == true)
+            {
+                choosePlay();
+            }
+
+        }
+
+        public void playPrevious()
+        {
+            if(isShuffled)
+            {
+                currentSong = rnd.Next() % playlist.Count;
+            }
+            else
+            {
+                if (currentSong == 0)
+                {
+                    currentSong = playlist.Count();
+                }
+                currentSong--;
+            }
+            mainWindow.Dispatcher.Invoke(() => {
+                mainWindow.timeSlider.Maximum = getSongTime(currentSong).TotalSeconds;
+                mainWindow.SongTime.Content = getSongTime(currentSong).ToString().Substring(3, 5);
+                mainWindow.timeSlider.Value = 0;
+                mainWindow.SongName.Content = getName(currentSong);
+            });
+            if (isPlaying == true)
+            {
+                choosePlay();
+            }
+
+        }
+        #endregion
+
+        #region ZMIANA GŁOŚNOŚCI
+        public void setVolume(float volume)
+        {
+            if (_soundOut != null)
+            {
+                _soundOut.Volume = volume;
+            }
+            else
+                this.volume = volume;
+        }
+        #endregion
+
+        #region PLAYLIST
+        public List<string> getPlaylist()
+        {
+            return playlist;
+        }
+        public int getPlaylistSize()
+        {
+            return playlist.Count();
+        }
+
+        public void savePlaylist(string name)
+        {
+            File.WriteAllLines(name + ".plr", playlist);
+        }
+
+        public void addToPlaylist(string path)
+        {
+            playlist.Add(path);
+            MediaFoundationDecoder decoder = new Mp3MediafoundationDecoder(path);
+            songsTime.Add(decoder.GetLength());
+        }
+
+
+        public void loadPlaylist(string path)
+        {
+            if (Path.GetExtension(path) == ".plr")
+            {
+                currentSong = 0;
+                songsTime.Clear();
+                playlist.Clear();
+                var files = File.ReadAllLines(path);
+                foreach (var s in files)
+                {
+                    if (File.Exists(s))
+                    {
+                        playlist.Add(s);
+                        MediaFoundationDecoder decoder = new Mp3MediafoundationDecoder(s);
+                        songsTime.Add(decoder.GetLength());
+                    }
+                }
+            }
+        }
+        #endregion
+    }
+}
